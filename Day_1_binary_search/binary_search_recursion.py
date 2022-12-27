2f3b7c7df32d25f3588b80b3c50e4ee4b52efc89
Mohit <mohitsoni.sh@gmail.com>
2022-12-27 15:34:42 +0530

    recursion

---
+++ Day_1_binary_search/binary_search_recursion.py
@@ -0,0 +1,16 @@
+#same with recursion
+
+def BS_recursion(ls, start, end, target):
+    mid = (start + end) //2
+
+    if ls[mid] == target:
+        return mid +1
+    elif (ls[mid] > target):
+        return BS_recursion(ls, start, mid -1, target)
+    elif (ls[mid] < target):
+        return BS_recursion(ls, mid + 1, end, target)
+    else:
+        return -1
+
+ls = [11,23,31,45,56,62,78,84,96]
+print(BS_recursion(ls, 0, len(ls)-1, 78))
